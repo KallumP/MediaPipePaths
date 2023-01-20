6ee81e25a15f0b24e30fb4c627d34144e15904c7
Kallum Panditharatna <kallum179@gmail.com>
2023-01-20 08:47:35 +0000

    :art: Moved functions to a helper class

---
+++ src/GestureEditor.py
@@ -0,0 +1,9 @@
+# need two windows
+# one for the index and type select
+# one for the camera output
+# v1
+# the selector needs to show a text box for index and type
+
+# the camera output needs to show the same as the previous editor
+# and call the same get angle, get position functions as the tracker
+
--- src/GestureTracker.py
+++ src/GestureTracker.py
@@ -4,74 +4,7 @@ import json
 from termcolor import colored
 import math
 import time
-
-
-# Returns the distance between two points
-def GetDistance(start, end):
-    xDistance2 = (start[0] - end[0]) ** 2
-    yDistance2 = (start[1] - end[1]) ** 2
-
-    distance = (xDistance2 + yDistance2) ** 0.5
-    return distance
-
-
-# gets the angle between three points (the angle of the middle parameter)
-def GetAnglePoints(trackStart, trackMid, trackEnd):
-    aLength = GetDistance(trackStart, trackMid)
-    bLength = GetDistance(trackMid, trackEnd)
-    cLength = GetDistance(trackEnd, trackStart)
-
-    return GetAngleLengths(aLength, bLength, cLength)
-
-
-# gets the angle of three lengths using cosine rule (angle opposite length C)
-def GetAngleLengths(a, b, c):
-    # derivation of cosine rule
-    # c^2 = a^2+b^2 - 2ab Cos(C)
-    # 0 = a^2+b^2 - c^2 - 2ab Cos(C)
-    # 2ab Cos(C) = a^2+b^2 - c^2
-    # Cos(C) = (a^2+b^2 - c^2) / 2ab
-    # C = Cos-1( (a^2+b^2 - c^2) / 2ab)
-
-    C_rad = math.acos((a ** 2 + b ** 2 - c ** 2) / (2 * a * b))
-    C_deg = math.degrees(C_rad)
-    return C_deg
-
-
-# returns if the user's points are within the target angle
-def WithinAngle(index, point):
-    toTrack = point.get("toTrack")
-    targetAngle = point.get("angle")
-    leniency = point.get("leniency")
-
-    # different points to track
-    start = [results.pose_landmarks.landmark[toTrack[0]].x, results.pose_landmarks.landmark[toTrack[0]].y]
-    mid = [results.pose_landmarks.landmark[toTrack[1]].x, results.pose_landmarks.landmark[toTrack[1]].y]
-    end = [results.pose_landmarks.landmark[toTrack[2]].x, results.pose_landmarks.landmark[toTrack[2]].y]
-
-    pointsAngle = GetAnglePoints(start, mid, end)
-
-    # if the angle of the three points are that of the target
-    if pointsAngle > targetAngle - leniency and pointsAngle < targetAngle + leniency:
-        return True
-    return False
-
-
-# checks if a user's index is within a target position
-def WithinTarget(index, point):
-    toTrack = point.get("toTrack")
-    leniency = point.get("leniency")
-    targetPosition = [point.get("target")[0], point.get("target")[1]]
-
-    # gets the point of the index to be tracked
-    indexPosition = [results.pose_landmarks.landmark[toTrack].x, results.pose_landmarks.landmark[toTrack].y]
-
-    distance = GetDistance(indexPosition, targetPosition)
-
-    if distance < leniency:
-        return True
-    return False
-
+from helper import *
 
 # Deals with tracking the next keyframe of the gesture file
 def TrackKeyframe(index, keyframes):
@@ -90,11 +23,11 @@ def TrackKeyframe(index, keyframes):
         # gets the point type
         pointType = point.get("pointType")
         if (pointType == "triPointAngle"):
-            if not WithinAngle(index, point):
+            if not WithinAngle(index, point, results):
                 allPassed = False
                 break
         elif (pointType == "pointPosition"):
-            if not WithinTarget(keyFrameIndex, point):
+            if not WithinTarget(keyFrameIndex, point, results):
                 allPassed = False
                 break
         else:
@@ -123,9 +56,7 @@ def TrackKeyframe(index, keyframes):
 
 
 mp_pose = mp.solutions.pose
-pose_model = mp_pose.Pose(
-    min_detection_confidence=0.5,
-    min_tracking_confidence=0.5)
+pose_model = mp_pose.Pose()
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
